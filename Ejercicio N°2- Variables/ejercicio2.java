f6a77b44bd2327057f01f9efedc8b14051054bfc
PatricioVisconti <visconti.patricio@gmail.com>
2020-09-22 18:27:03 -0300

    Ejercicio 2

---
+++ Ejercicio N°2- Variables/ejercicio2.java
@@ -0,0 +1,10 @@
+public class ejercicio2 {
+	public static void main(String args[]){
+		//Declaracion
+		int numero1 = 5, numero2 = 10, resultado = 0;
+		//Operacion
+		resultado = numero1 + numero2;
+		//Print
+		System.out.println("El resultado de la suma "+numero1+" y "+numero2+" es igual a "+resultado);
+	}
+}
